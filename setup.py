f229cc5e1bdacbd4c224c94468a7a15ef1f23645
hramon <4901782+hramon@users.noreply.github.com>
2021-05-09 13:49:25 +0200

    Added long description to setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 from distutils.core import setup
 
+with open("README.md", "r") as file:
+    long_description = file.read()
+
 setup(name='symosi',
-      version='1.0',
-      description='Python System Model Simulation Library',
-      author='hramon',
-      packages=['symosi'],
-      install_requires=[
-            'numpy',
-            'scipy'
-      ]
-     )
+    version='1.0',
+    description='Python System Model Simulation Library',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='hramon',
+    packages=['symosi'],
+    install_requires=[
+        'numpy',
+        'scipy'
+    ]
+)
