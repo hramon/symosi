e7fd93798de108bac80db00151dcb43eee661f10
hramon <4901782+hramon@users.noreply.github.com>
2021-05-09 14:02:23 +0200

    setup.py setuptools instead of distutils.core

--- setup.py
+++ setup.py
@@ -1,12 +1,9 @@
-from distutils.core import setup
-
-with open("README.md", "r") as file:
-    long_description = file.read()
+from setuptools import setup
 
 setup(name='symosi',
     version='1.0',
     description='Python System Model Simulation Library',
-    long_description=long_description,
+    long_description=open("README.md", "r").read(),
     long_description_content_type="text/markdown",
     author='hramon',
     packages=['symosi'],
